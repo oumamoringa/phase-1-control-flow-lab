7f4ac66ade17f7e47d8388d428a3677968dc1868
oumamoringa <richard.ouma@student.moringaschool>
2022-08-16 11:04:29 +0300

    New changes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if ("distance" <= "400 feet")
+    return "free ride";
+  else if ("distance" > "200 feet")
+    return "30 dollars";
+  else
+    return "Does not allow ride";
+
 }
 
 function ternaryCheckCity(){
